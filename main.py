17bbb359ff9c32194fe5b862e7973a86b5891599
user <ponamarvdanil@gmail.com>
2022-08-04 00:50:38 +0700

    Finish version

--- main.py
+++ main.py
@@ -20,27 +20,29 @@ def quotient(number_1: int, number_2: int) -> float:
 
 def calculator(mode: int):
     print("enter two numbers")
-    first_number = int(input())
-    second_number = int(input())
-    if mode == 1:
-        print(summa(first_number, second_number))
-    elif mode == 2:
-        print(difference(first_number, second_number))
-    elif mode == 3:
-        print(multy(first_number, second_number))
-    elif mode == 4:
-        res = quotient(first_number, second_number)
-        if res == -1:
-            print("bad instruction")
-        else:
-            print(res)
+    try:
+        first_number = int(input())
+        second_number = int(input())
+    except ValueError:
+        print("it is not integer\n")
     else:
-        print("We doesn't know what is it")
+        if mode == 1:
+            print(summa(first_number, second_number), "\n")
+        elif mode == 2:
+            print(difference(first_number, second_number), "\n")
+        elif mode == 3:
+            print(multy(first_number, second_number), "\n")
+        elif mode == 4:
+            res = quotient(first_number, second_number)
+            if res == -1:
+                print("bad instruction\n")
+            else:
+                print(res, "\n")
 
 
 if __name__ == '__main__':
     work = 0
-    while work != -1:
+    while work != 1:
         print("press '1' to calculate the sum")
         print("press '2' to calculate the difference")
         print("press '3' to calculate the multiplication")
@@ -48,6 +50,15 @@ if __name__ == '__main__':
         print("press 'q' to exit")
         behavior = input()
         if behavior == 'q':
-            work = -1
+            work = 1
         else:
-            calculator(int(behavior))
+            modes = ['1', '2', '3', '4']
+            flag = 0
+            for i in modes:
+                if behavior == i:
+                    flag = 1
+                    break
+            if flag == 1:
+                calculator(int(behavior))
+            else:
+                print("We doesn't know what need to do\n")
